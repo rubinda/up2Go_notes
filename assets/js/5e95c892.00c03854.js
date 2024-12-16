5d63c8cab3b741f41cb3efb78933e506803121a7
David <david.rubin@up2go.com>
2024-12-16 14:20:51 +0100

    Deploy website - based on 2b4ab021d1f6ed934b81c9e4b870d4ce9431c0d1

---
+++ assets/js/5e95c892.00c03854.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkup2go_notes=self.webpackChunkup2go_notes||[]).push([[647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>p});r(6540);var u=r(4164),n=r(1003),t=r(7559),a=r(2831),c=r(781),o=r(4848);function p(e){return(0,o.jsx)(n.e3,{className:(0,u.A)(t.G.wrapper.docsPages),children:(0,o.jsx)(c.A,{children:(0,a.v)(e.route.routes)})})}}}]);
